93e12a936ed95d5f90a4892f2ddfb7f023ebc55d
GeodiyaRuxan <geodiya@gmail.com>
2025-05-30 16:34:42 +0530

    Initial commit :Password Strength checker

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request
+
+import re
+
+app = Flask(__name__)
+
+def check_password_strength(password):
+    score = 0
+    feedback = []
+
+    if len(password) >= 8:
+        score += 1
+    else:
+        feedback.append("Password should be at least 8 characters long.")
+
+    if re.search(r"[A-Z]", password):
+        score += 1
+    else:
+        feedback.append("Add at least one uppercase letter.")
+
+    if re.search(r"[a-z]", password):
+        score += 1
+    else:
+        feedback.append("Add at least one lowercase letter.")
+
+    if re.search(r"\d", password):
+        score += 1
+    else:
+        feedback.append("Add at least one digit.")
+
+    if re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+        score += 1
+    else:
+        feedback.append("Add at least one special character (!@#$%^&* etc).")
+
+    return score, feedback
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    result = None
+    feedback = []
+    if request.method == "POST":
+        password = request.form.get("password")
+        score, feedback = check_password_strength(password)
+        result = {"score": score, "max_score": 5, "feedback": feedback}
+
+    return render_template("index.html", result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
